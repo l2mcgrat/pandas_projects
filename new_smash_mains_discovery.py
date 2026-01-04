8a67f2e867ce0585845fba6395d5d98350eba091
Liam McGrath <l2mcgrat@uwaterloo.ca>
2026-01-04 14:56:39 -0500

    Added New Records, 13, 14

--- new_smash_mains_discovery.py
+++ new_smash_mains_discovery.py
@@ -4717,9 +4717,27 @@ max_percentage = 200
 blank_dict = {character:[] for character in round_12_scores_dict}
 Tourneys = [Tourney_List_12]
 round_12_records = records(Tourneys, blank_dict, max_percentage)
-round_12_records["Score"] = pd.to_numeric(round_11_records["Score"], errors="coerce")
+round_12_records["Score"] = pd.to_numeric(round_12_records["Score"], errors="coerce")
 round_12_records["Accumulated_Sum"] = round_12_records.groupby("Character")["Score"].cumsum()
 round_12_records.to_csv("records/round_12_records.csv", index=False)
+
+# Round 13 Only
+max_percentage = 200
+blank_dict = {character:[] for character in round_13_scores_dict}
+Tourneys = [Tourney_List_13]
+round_13_records = records(Tourneys, blank_dict, max_percentage)
+round_13_records["Score"] = pd.to_numeric(round_13_records["Score"], errors="coerce")
+round_13_records["Accumulated_Sum"] = round_13_records.groupby("Character")["Score"].cumsum()
+round_13_records.to_csv("records/round_13_records.csv", index=False)
+
+# Round 14 Only
+max_percentage = 200
+blank_dict = {character:[] for character in round_14_scores_dict}
+Tourneys = [Tourney_List_14]
+round_14_records = records(Tourneys, blank_dict, max_percentage)
+round_14_records["Score"] = pd.to_numeric(round_14_records["Score"], errors="coerce")
+round_14_records["Accumulated_Sum"] = round_14_records.groupby("Character")["Score"].cumsum()
+round_14_records.to_csv("records/round_14_records.csv", index=False)
 
 # All Rounds
 max_percentage = 200
